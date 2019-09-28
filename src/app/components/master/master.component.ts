5582c4cdbb62b9771bec68839d47b1d1188d63e6
Marcelo Puebla <marcenp23@gmail.com>
2019-09-28 16:15:42 -0300

    Fade anim en master component.

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from "@angular/animations";
 
 @Component({
   selector: "app-master",
   templateUrl: "./master.component.html",
-  styleUrls: ["./master.component.scss"]
+  styleUrls: ["./master.component.scss"],
+  animations: [
+    trigger("fadeAnimation", [
+      state("in", style({ opacity: 1 })),
+      transition(":enter", [style({ opacity: 0 }), animate(600)]),
+      transition(":leave", animate(600, style({ opacity: 0 })))
+    ])
+  ]
 })
 export class MasterComponent implements OnInit {
   constructor() {}
