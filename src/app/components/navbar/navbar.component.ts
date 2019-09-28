acdc8948de6f65825416c4b7576c52e7c10fc0a9
Marcelo Puebla <marcenp23@gmail.com>
2019-09-28 18:29:45 -0300

    Arreglo y agregado en navbar.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html'
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
-
 export class navbarComponent implements OnInit {
-    constructor() {
+  public solidNavbar: boolean = false;
 
+  constructor() {}
+
+  ngOnInit() {
+    console.log(window.innerWidth);
+  }
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowsScroll($event: Event) {
+    let number =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (number > 100) {
+      this.solidNavbar = true;
+    } else if (this.solidNavbar && number < 10) {
+      this.solidNavbar = false;
     }
-    ngOnInit;
+  }
 }
