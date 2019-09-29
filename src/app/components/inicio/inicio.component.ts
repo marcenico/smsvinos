a235efb9491f000af34f6c9cbb253df1c4a889ae
Marcelo Puebla <marcenp23@gmail.com>
2019-09-29 01:04:37 -0300

    Arreglo en footer y en paginas de inicio.

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  selector: "app-inicio",
+  templateUrl: "./inicio.component.html",
+  styleUrls: ["./inicio.component.scss"],
+  animations: [
+    trigger("fadeAnimation", [
+      state("in", style({ opacity: 1 })),
+
+      transition(":enter", [style({ opacity: 0 }), animate(600)]),
+
+      transition(":leave", animate(600, style({ opacity: 0 })))
+    ])
+  ]
 })
 export class InicioComponent implements OnInit {
   private ageConfirmed: boolean = false;
 
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   confirmed() {
     this.ageConfirmed = true;
   }
