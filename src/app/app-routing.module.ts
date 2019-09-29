7f8ca31d04acc458fc769ce2c7d33c4fe8274688
Marcelo Puebla <marcenp23@gmail.com>
2019-09-29 12:59:29 -0300

    Arreglo para que funcionen las rutas en produccion.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { BodegaComponent } from './components/bodega/bodega.component';
-import { ViniedosComponent } from './components/viniedos/viniedos.component';
-import { NuestrosViniedosComponent } from './components/nuestros-viniedos/nuestros-viniedos.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { MasterComponent } from './components/master/master.component';
-import { HomeComponent } from './components/home/home.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { BodegaComponent } from "./components/bodega/bodega.component";
+import { ViniedosComponent } from "./components/viniedos/viniedos.component";
+import { NuestrosViniedosComponent } from "./components/nuestros-viniedos/nuestros-viniedos.component";
+import { ContactoComponent } from "./components/contacto/contacto.component";
+import { MasterComponent } from "./components/master/master.component";
+import { HomeComponent } from "./components/home/home.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
-  { path: 'inicio', component: InicioComponent },
+  { path: "", pathMatch: "full", redirectTo: "inicio" },
+  { path: "inicio", component: InicioComponent },
   {
-    path: '', component: MasterComponent, children: [
-
-      { path: 'home', component: HomeComponent },
-      { path: 'bodega', component: BodegaComponent },
-      { path: 'viniedos', component: ViniedosComponent },
-      { path: 'nuestrosViñedos', component: NuestrosViniedosComponent },
-      { path: 'contacto', component: ContactoComponent },
+    path: "",
+    component: MasterComponent,
+    children: [
+      { path: "home", component: HomeComponent },
+      { path: "bodega", component: BodegaComponent },
+      { path: "viniedos", component: ViniedosComponent },
+      { path: "nuestrosViñedos", component: NuestrosViniedosComponent },
+      { path: "contacto", component: ContactoComponent }
     ]
   },
-  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
-
+  { path: "**", pathMatch: "full", redirectTo: "inicio" }
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { BodegaComponent } from './components/bodega/bodega.component';
-import { navbarComponent } from './components/navbar/navbar.component';
-import { from } from 'rxjs';
-import { ViniedosComponent } from './components/viniedos/viniedos.component';
-import { NuestrosViniedosComponent } from './components/nuestros-viniedos/nuestros-viniedos.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { HomeComponent } from './components/home/home.component';
-import { MasterComponent } from './components/master/master.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { BodegaComponent } from "./components/bodega/bodega.component";
+import { navbarComponent } from "./components/navbar/navbar.component";
+import { ViniedosComponent } from "./components/viniedos/viniedos.component";
+import { NuestrosViniedosComponent } from "./components/nuestros-viniedos/nuestros-viniedos.component";
+import { ContactoComponent } from "./components/contacto/contacto.component";
+import { HomeComponent } from "./components/home/home.component";
+import { MasterComponent } from "./components/master/master.component";
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,12 +26,8 @@ import { MasterComponent } from './components/master/master.component';
     MasterComponent,
     HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
